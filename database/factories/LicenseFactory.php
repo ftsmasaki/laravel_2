e72f8f4ef4f254ce787ab9b1e0133c63f862a626
ftsmasaki <masaki@fts-com.jp>
2022-02-28 14:15:31 +0900

    ライセンスをFakerで作成

---
+++ database/factories/LicenseFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\License;
+
+class LicenseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $product_key = $this->faker->bothify('*****-*****-*****-*****-*****');
+
+        return [
+            'product_name' => $this->faker->randomElement(['Microsoft Office Home & Business 2021']),
+            'product_key' => $this->faker->toUpper($product_key),
+            'expire_date' => $this->faker->dateTimeBetween('+1 year', '+5 year'),
+            'purchase_date' => $this->faker->dateTimeBetween('-5 year', '-1 week'),
+            'is_notify' => $this->faker->numberBetween(0,1),
+        ];
+    }
+}
--- database/seeders/LicensesTableSeeder.php
+++ database/seeders/LicensesTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\License;
 
 class LicensesTableSeeder extends Seeder
 {
@@ -17,18 +18,22 @@ class LicensesTableSeeder extends Seeder
         //テーブルのクリア
         DB::table('licenses')->truncate();
 
-        //初期データ用意
-        $licenses = [
-            ['product_name' => 'ESET Internet Security',
-            'product_key' => 'ABC1-ABCD1234',
-            'expire_date' => '2022-02-28',
-            'purchase_date' => '2021-03-01',
-            'is_notify' => true],
-        ];
+        //データを手動で作成
+        // //初期データ用意
+        // $licenses = [
+        //     ['product_name' => 'ESET Internet Security',
+        //     'product_key' => 'ABC1-ABCD1234',
+        //     'expire_date' => '2022-02-28',
+        //     'purchase_date' => '2021-03-01',
+        //     'is_notify' => true],
+        // ];
 
-        //登録
-        foreach($licenses as $license) {
-            \App\Models\License::create($license);
-        }
+        // //登録
+        // foreach($licenses as $license) {
+        //     \App\Models\License::create($license);
+        // }
+
+        //データをFakerで自動作成
+        License::factory()->count(15)->create();
     }
 }
