b93978ec082d6b321d24b0b4e189182ca7084290
ftsmasaki <masaki@fts-com.jp>
2022-03-02 01:08:45 +0900

    データベースを再作成

--- database/migrations/2022_03_02_000148_create_licenses_table_2.php
+++ database/migrations/2022_03_02_000148_create_licenses_table_2.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLicensesTable extends Migration
+class CreateLicensesTable2 extends Migration
 {
     /**
      * Run the migrations.
