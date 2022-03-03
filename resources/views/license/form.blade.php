b49a2206a1f3bba2e7742117daf79a60a10b0d3e
ftsmasaki <masaki@fts-com.jp>
2022-03-03 17:46:33 +0900

    レイアウト調整

--- resources/views/license/form.blade.php
+++ resources/views/license/form.blade.php
@@ -10,34 +10,36 @@
         @endif
             <input type="hidden" name="_token" value="{{ csrf_token() }}">
             <div class="mb-4">
-                <div class="form-group">
-                    <label for="product_name">製品名</label>
-                    <input type="text" class="form-control" name="product_name" value="{{ $license->product_name }}">
+                <div class="form-group d-flex mb-2">
+                    <div class="col-md-2 d-flex align-items-center"><label for="product_name">製品名</label></div>
+                    <div class="col-md-6"><input type="text" class="form-control" name="product_name" value="{{ $license->product_name }}"></div>
                 </div>
-                <div class="form-group">
-                    <label for="product_key">プロダクトキー</label>
-                    <input type="text" class="form-control" name="product_key" value="{{ $license->product_key }}">
+                <div class="form-group d-flex mb-2">
+                    <div class="col-md-2 d-flex align-items-center"><label for="product_key">プロダクトキー</label></div>
+                    <div class="col-md-6"><input type="text" class="form-control" name="product_key" value="{{ $license->product_key }}"></div>
                 </div>
-                <div class="form-group">
-                    <label for="expire_date">有効期限</label>
-                    <input type="date" class="form-control" name="expire_date" value="{{ $license->expire_date }}">
+                <div class="form-group d-flex mb-2">
+                    <div class="col-md-2 d-flex align-items-center"><label for="expire_date">有効期限</label></div>
+                    <div class="col-md-6"><input type="date" class="form-control" name="expire_date" value="{{ $license->expire_date }}"></div>
                 </div>
-                <div class="form-group">
-                    <label for="purchase_date">購入日</label>
-                    <input type="date" class="form-control" name="purchase_date" value="{{ $license->purchase_date }}">
+                <div class="form-group d-flex mb-2">
+                    <div class="col-md-2 d-flex align-items-center"><label for="purchase_date">購入日</label></div>
+                    <div class="col-md-6"><input type="date" class="form-control" name="purchase_date" value="{{ $license->purchase_date }}"></div>
                 </div>
-                <div class="form-group">
-                    <label for="seats">割当数</label>
-                    <input type="number" class="form-control" name="seats" value="{{ $license->seats }}">
+                <div class="form-group d-flex mb-2">
+                    <div class="col-md-2 d-flex align-items-center"><label for="seats">割当数</label></div>
+                    <div class="col-md-6"><input type="number" class="form-control" name="seats" value="{{ $license->seats }}"></div>
                 </div>
-                <div class="form-group">
-                    <label for="is_notify">通知</label>
-                    <input type="hidden" class="form-check-input" name="is_notify" value="0">
-                    <input type="checkbox" class="form-check-input" name="is_notify" {{ $license->is_notify == '1' ? ' checked="checked"' : '' }}" value="1">
+                <div class="form-group d-flex mb-2">
+                    <div class="col-md-2 d-flex align-items-center"><label for="is_notify">メール通知</label></div>
+                    <div class="col-md-6">
+                        <input type="hidden" class="form-check-input" name="is_notify" value="0">
+                        <input type="checkbox" class="form-check-input" name="is_notify" {{ $license->is_notify == '1' ? ' checked="checked"' : '' }} value="1">
+                    </div>
                 </div>
             </div>
 
-            <div class="mb-4 text-end">
+            <div class="col-md-8 mb-4 text-end">
                 <button type="submit" class="btn btn-primary">登録</button>
                 <a href="/license" class="btn btn-primary">戻る</a>
             </div>
